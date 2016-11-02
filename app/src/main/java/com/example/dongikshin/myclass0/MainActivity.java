ee57cbef7d81a485ba50f24728af83453ed87151
sdi654321 <ehddlr0901@gmail.com>
2016-11-02 17:34:45 +0900

    첫번째

--- app/src/main/java/com/example/dongikshin/myclass0/MainActivity.java
+++ app/src/main/java/com/example/dongikshin/myclass0/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButton2Clicked(View v) {
-        person1.walk(20);
+        person1.walk(30);
     }
 
     public void onButton3Clicked(View v) {
