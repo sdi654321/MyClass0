d6a43018b509a40d228bb1d3826bcde628d821b2
sdi654321 <ehddlr0901@gmail.com>
2016-11-03 19:55:47 +0900

    네번째욥

--- app/src/main/java/com/example/dongikshin/myclass0/MainActivity.java
+++ app/src/main/java/com/example/dongikshin/myclass0/MainActivity.java
@@ -13,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
     EditText editText2;
     ImageView imageView;
     person person1;
-
+    public static int total=1;
+    public int age =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +34,16 @@ public class MainActivity extends AppCompatActivity {
         if(personChecked) {
             person1 = new person(name);
             imageView.setImageResource(R.drawable.person);
-
-                Toast.makeText(getApplicationContext(), "아기가 성장했어요.." + name, Toast.LENGTH_LONG).show();
-
+                Toast.makeText(getApplicationContext(), "어른을 만들었어요.. 이름은 " + name, Toast.LENGTH_LONG).show();
+            age = 20;
         }
         else
         {
             person1 = new baby(name);
             imageView.setImageResource(R.drawable.baby);
+            age = 0;
         }
-
+        Toast.makeText(getApplicationContext(),total +" 번 째 사람", Toast.LENGTH_LONG).show();
     }
 
     public void onButton2Clicked(View v) {
@@ -57,34 +58,62 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void onButton4Clicked(View v) {
+        if(person1 instanceof baby) {
+            ((baby) person1).smile();
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"이 어른은 웃지 않아요", Toast.LENGTH_LONG).show();
+        }
+    }
 
     public class person {
         String name;
+
         public person() {
         }
 
         public person(String name) {
             this.name = name;
+            total = total +1;
         }
 
         public void walk(int speed) {
-            imageView.setImageResource(R.drawable.person_walk);
-            Toast.makeText(getApplicationContext(), name + "이(가) 걸어갑니다", Toast.LENGTH_LONG).show();
+            if(age == 20) {
+                imageView.setImageResource(R.drawable.person_walk);
+                Toast.makeText(getApplicationContext(), name + "이(가) 걸어갑니다", Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(), name + "는 아기라 걷지 못해요", Toast.LENGTH_LONG).show();
+            }
         }
 
         public void run(int speed) {
-            imageView.setImageResource(R.drawable.person_run);
-            Toast.makeText(getApplicationContext(), name + "이(가) 뛰어갑니다", Toast.LENGTH_LONG).show();
+            if(age == 20) {
+                imageView.setImageResource(R.drawable.person_run);
+                Toast.makeText(getApplicationContext(), name + "이(가) 뛰어갑니다", Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(getApplicationContext(), name + "는 아기라 뛰지 못해요", Toast.LENGTH_LONG).show();
+            }
 
         }
     }
-   public class baby extends person{
-        public baby(String name){
-            super.name = name;
-            Toast.makeText(getApplicationContext(),"아기를 만들었어요.." + name, Toast.LENGTH_LONG).show();
 
+        public class baby extends person {
+            public baby(String name) {
+                super.name = name;
+                Toast.makeText(getApplicationContext(), "아기를 만들었어요.. 이름은"  + name, Toast.LENGTH_LONG).show();
+
+            }
+
+            public void smile() {
+                imageView.setImageResource(R.drawable.baby_smile);
+                Toast.makeText(getApplicationContext(), "아기가 웃어요" + name, Toast.LENGTH_LONG).show();
+            }
         }
     }
-}
+
 
 
